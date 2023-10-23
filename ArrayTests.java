4e703c7695520b64b890b4de8d8046e2d767e6db
JustCode65 <justkim1128@gmail.com>
2023-10-23 13:07:27 -0700

    updates

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,6 +9,18 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 1,2,3,4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 4,3,2,1 }, input1);
+	}
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 5,6,7,8,9 };
+    assertArrayEquals(new int[]{ 9,8,7,6,5 }, ArrayExamples.reversed(input1));
+  }
 
   @Test
   public void testReversed() {
